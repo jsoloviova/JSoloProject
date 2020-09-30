fbca01c375267afe165ce24e41f9aa53160b28ee
jsoloviova <iulia.tdl@gmail.com>
2020-09-30 23:51:42 +0300

    print out summ of ints

--- src/main/java/JSMain3Renamed.java
+++ src/main/java/JSMain3Renamed.java
@@ -1,9 +1,14 @@
 public class JSMain3Renamed {
     public static void main(String[] args) {
+        int j = 11;
+        int k = 13;
+        int s = 16;
         System.out.println("all you need is Git! ");
+        System.out.println(j + k + s);
     }
 
-    int j = 11;
-    int k = 13;
-    int s = 16;
+
+
+
+    }
 }
