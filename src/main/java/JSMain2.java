bc13daf1de53ffea0005791ce20f5d90f0779e43
jsoloviova <iulia.tdl@gmail.com>
2020-09-30 20:43:48 +0300

    add new variable - str2

--- src/main/java/JSMain2.java
+++ src/main/java/JSMain2.java
@@ -2,5 +2,7 @@ public class JSMain2 {
     public static void main(String[] args) {
         System.out.println("learnig Git!");
         System.out.println("YAY!!");
+
+        String str2 = "Boom";
     }
 }
