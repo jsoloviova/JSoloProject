814a346a3d69bb407026f0878ba4f30852fc0c0a
jsoloviova <iulia.tdl@gmail.com>
2020-09-30 23:50:26 +0300

    new ints added to JSMain3Renamed

--- src/main/java/JSMain3Renamed.java
+++ src/main/java/JSMain3Renamed.java
@@ -1,7 +1,9 @@
 public class JSMain3Renamed {
     public static void main(String[] args) {
-        System.out.println("all you need is Git! that's for sure");
+        System.out.println("all you need is Git! ");
     }
 
     int j = 11;
+    int k = 13;
+    int s = 16;
 }
