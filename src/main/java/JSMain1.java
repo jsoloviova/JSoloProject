0b55afb10f1b2d8b074b3fa32f9b2686cab8ea00
jsoloviova <iulia.tdl@gmail.com>
2020-09-30 14:51:27 +0300

    Initial commit

---
+++ src/main/java/JSMain1.java
@@ -0,0 +1,7 @@
+public class JSMain1 {
+    static int x = 8;
+
+    public static void main(String[] args) {
+        System.out.println(x * 3);
+    }
+}
---
+++ src/main/java/JSMain2.java
@@ -0,0 +1,5 @@
+public class JSMain2 {
+    public static void main(String[] args) {
+        System.out.println("learnig Git!");
+    }
+}
