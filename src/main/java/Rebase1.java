e0e6d789ffc5dafffc604f99587e7f0d564b5470
jsoloviova <iulia.tdl@gmail.com>
2020-10-01 12:15:45 +0300

    master rebase 2nd commit

--- src/main/java/Rebase1.java
+++ src/main/java/Rebase1.java
@@ -1,5 +1,6 @@
 public class Rebase1 {
     public static void main(String[] args) {
-        System.out.println("master rebase 1 commit");
+        System.out.println("master rebase 1st commit");
+        System.out.println("master rebase 2nd commit");
     }
 }
